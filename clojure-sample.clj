58b9fcb70a5f111b3c30f6a0dcfa75bb4d051dab
Barry Bryant <barry.bryant@willowtreeapps.com>
2017-08-07 10:50:16 -0400

    testerooni

--- clojure-sample.clj
+++ clojure-sample.clj
@@ -1,4 +1,28 @@
 (fn [state time-left]
+  (ns i-am-a-golden-god.core
+  (:require [clj-http.client :as client]
+            [clojure.data.json :as json]))
+
+ (defn extractJson
+  [responseBody]
+  (get (json/read-str responseBody) "value1"))
+
+ (defn makeCall
+  [url input]
+   (:body
+    (client/get url
+                {:query-params {"value1" input}})))
+
+ (defn getIt
+  [url input]
+   (println
+    (extractJson
+     (makeCall url input))))
+
+ (defn run [] (getIt "http://ermahgerd.herokuapp.com/ternslert" 
+                      "I am become Kelsey, destroyer of worlds"))
+
+ (run)
   (def turn-directions [:right :left :about-face])
   (def smoke-directions [:forward :backward :left :right :drop])
 
